6df8c9481369befc08f1db82e13819270b19e2a1
Max Hernandez <maxhernandez042003@gmail.com>
2025-02-27 12:50:59 -0600

    Señales de trading

---
+++ Functions/Trading_signals.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def trading_signals (spread_norm, data, tickers):
+
+    umbral = 1.5
+
+    signals = np.zeros(len(spread_norm))
+
+    signals[spread_norm > umbral] = -1 # Short cuando el spread supera +1.5
+    signals[spread_norm < umbral] = 1 # Long cuando el spread cae por debajo de -1.5
+    signals[(spread_norm < umbral) & (spread_norm > -umbral)] = 0 # Cierre de posición
+
+    df_signal = data.copy()
+    df_signal["Señales"] = signals
+
+    return df_signal
+
+def spread_signals(spread_norm, df_signal):
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(spread_norm, label = "Spread Normalizado", color = "blue")
+    ax.axhline(y = 1.5, color = "orange", linestyle = "-", linewidth= 1, label = "1.5 sigma")
+    ax.axhline(y = -1.5, color = "orange", linestyle="-", linewidth = 1)
+
+    ax.scatter(df_signal.index[df_signal["Señales"] == 1], spread_norm[df_signal["Señales"] == 1],
+               color = "green", marker = "^", label = "Long", alpha = 1, s=80)
+    ax.scatter(df_signal.index[df_signal["Señales"] == -1], spread_norm[df_signal["Señales"] == -1],
+               color = "red", marker = "v", label = "Short", alpha = 1, s=80)
+
+    ax.set_xlabel("Fecha")
+    ax.set_ylabel("Spread Normalizado")
+    ax.set_title("Spread Normalizado con Señales de Trading")
+    ax.legend()
+    ax.grid()
+    plt.show()
+
+def prices_signals(data, df_signal, tickers):
+
+    fig, ax = plt.subplots(figsize = (12,6))
+
+    ax.plot(data.index, data[tickers[0]], label=tickers[0], color="blue")
+    ax.plot(data.index, data[tickers[1]], label=tickers[1], color="orange")
+
+    ax.scatter(df_signal.index[df_signal["Señales"] == 1],
+               data[tickers[0]][df_signal["Señales"] == 1],
+               color= "green", marker = "^", label = f"Long {tickers[0]}", alpha = 1, s= 80)
+
+    ax.scatter(df_signal.index[df_signal["Señales"] == 1],
+               data[tickers[1]][df_signal["Señales"] == 1],
+               color="red", marker="v", label=f"Short {tickers[1]}", alpha=1, s=80)
+
+    ax.scatter(df_signal.index[df_signal["Señales"] == -1],
+               data[tickers[0]][df_signal["Señales"] == -1],
+               color="red", marker="v", label=f"Short {tickers[0]}", alpha=1, s=80)
+
+    ax.scatter(df_signal.index[df_signal["Señales"] == -1],
+               data[tickers[1]][df_signal["Señales"] == -1],
+               color="green", marker="^", label=f"Long {tickers[1]}", alpha=1, s=80)
+
+    ax.set_xlabel("Fecha")
+    ax.set_ylabel("Precio")
+    ax.set_title(f"Precios de {tickers[0]} y {tickers[1]} con Señales de Trading")
+    ax.legend()
+    ax.grid()
+    plt.show()
+
+
+
--- Functions/main.py
+++ Functions/main.py
@@ -3,6 +3,7 @@ from Prueba_DickeyFuller import verificar_estacionaridad_pares
 from Prueba_EngleGranger import regresion_ols, prueba_estacionaridad_residuos
 from Prueba_Johanssen import prueba_johansen
 from Kalman_filter import (calcular_hedge_ratio_kalman, plot_hedge_ratio,ultimo_hedge_ratio,promedio_hedge_ratio)
+from Trading_signals import (trading_signals, spread_signals, prices_signals)
 
 
 def main():
@@ -23,6 +24,11 @@ def main():
     ultimo_hedge_ratio(hedge_ratios)
     promedio_hedge_ratio(hedge_ratios)
     plot_hedge_ratio(hedge_ratios)
+
+    df_signal = trading_signals(spread_norm, data, tickers)
+    spread_signals(spread_norm, df_signal)
+    prices_signals(data, df_signal, tickers)
+
 
 
 if __name__ == "__main__":
