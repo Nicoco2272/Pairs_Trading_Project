9ed80d3dfb99981c3d30046b4676b2d07ea70dce
Nicolas Ramirez Castro <ramirezcastronicolas@gmail.com>
2025-02-27 00:57:09 -0600

    Cambio de activos

--- Functions/main.py
+++ Functions/main.py
@@ -2,15 +2,15 @@ from Descarga_activos import descargar_datos, graficar_datos, normalizar_datos
 from Prueba_DickeyFuller import verificar_estacionaridad_pares
 from Prueba_EngleGranger import regresion_ols, prueba_estacionaridad_residuos
 from Prueba_Johanssen import prueba_johansen
-from Kalman_filter import (calcular_hedge_ratio_kalman, plot_hedge_ratio)  # Importa las funciones del filtro de Kalman
+from Kalman_filter import (calcular_hedge_ratio_kalman, plot_hedge_ratio)
 
 
 def main():
-    tickers = ["V", "MA"]
+    tickers = ["DAL", "UAL"]
 
     data = descargar_datos(tickers)
     data_normalizada = normalizar_datos(data)
-    graficar_datos(data_normalizada, tickers)
+    graficar_datos(data, tickers)
 
     verificar_estacionaridad_pares(data)
 
